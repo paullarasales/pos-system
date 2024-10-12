a3660cfad49bd0a5404885ae200ac9e49fa0d700
paullarasales <ahmadzufukli@gmail.com>
2024-10-12 13:00:51 +0800

    first commit

---
+++ app/Models/Sales.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sales extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sale_id',
+        'customer_id',
+        'total_amount'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function saleItems()
+    {
+        return $this->hasMany(SaleItem::class);
+    }
+}
